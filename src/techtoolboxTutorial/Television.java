32388daed6772c98444ed65aced2c103400ae255
Mathias-Pc <mathias.rerup-dyberg@hotmail.com>
2021-03-29 01:41:11 +0200

    Fixed token reveal, whoooooooooooooooooooooooooooooooooooooops

--- src/techtoolboxTutorial/Television.java
+++ src/techtoolboxTutorial/Television.java
@@ -7,6 +7,9 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Television {
     public static JDA jda;
@@ -14,8 +17,8 @@ public class Television {
 
 
     //Main method, on start
-    public static void main(String[] args) throws LoginException, InterruptedException {
-        JDABuilder builder = JDABuilder.createDefault("Haha no way imma reveal this");
+    public static void main(String[] args) throws LoginException, InterruptedException, FileNotFoundException {
+        JDABuilder builder = JDABuilder.createDefault(getToken()); //Hahah deffinetly dident reveal token on the first 3 commits, thats for sure!
         jda = builder.build();
         //jda.getPresence().setStatus(OnlineStatus.IDLE);
         jda.getPresence().setActivity(Activity.watching("You all lose!"));
@@ -25,5 +28,10 @@ public class Television {
         System.out.println("Bot started!");
     }
 
+    public static String getToken() throws FileNotFoundException {
+        File file = new File("src/Information/Token");
+        Scanner fin = new Scanner(file);
+        return fin.nextLine();
+    }
 
 }
